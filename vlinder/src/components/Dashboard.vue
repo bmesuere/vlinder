1fbc85a4d4ad2a4bfb408258e2ad8d760a949ec6
Flor Delombaerde <flor.delombaerde@ugent.be>
2020-04-05 19:00:18 +0200

    extended example viz

--- vlinder/src/components/Dashboard.vue
+++ vlinder/src/components/Dashboard.vue
@@ -1,8 +1,18 @@
 <template>
     <div>
         <b-container style="height: 100%">
-            <b-row align-v="center" style="height: 100%">
-                <example-visualization style="padding: 5em"/>
+            <b-row align-h="center" style="padding: 5em">
+                <b-col>
+                    First station selection:
+                    <b-form-select v-model="selectedStation1" :options="options"/>
+                </b-col>
+                <b-col>
+                    Second station selection:
+                    <b-form-select v-model="selectedStation2" :options="options"/>
+                </b-col>
+            </b-row>
+            <b-row>
+                <example-visualization v-bind:selectedStation="selectedStation1" style="padding: 5em"/>
             </b-row>
         </b-container>
     </div>
@@ -10,17 +20,34 @@
 
 <script>
     import ExampleVisualization from "./ExampleVisualization";
+    import vlinderService from "../services/vlinderService";
 
     export default {
         name: "Dashboard",
         components: {
             ExampleVisualization
         },
-        created () {
+        created() {
             this.$store.dispatch('fetchLatestVlinderData');
             setInterval(() => {
                 this.$store.dispatch('fetchLatestVlinderData')
-            }, 300000)
+            }, 300000);
+
+            let self = this;
+            vlinderService.getStations().then(
+                d => {
+                    d.data.forEach(station => {
+                        self.options.push({value: station['id'], text: station['name']})
+                    })
+                }
+            )
+        },
+        data() {
+            return {
+                selectedStation1: '',
+                selectedStation2: '',
+                options: []
+            }
         }
     }
 </script>
