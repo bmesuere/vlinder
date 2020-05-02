55e7706d4da18f12bc9690ba6aa78f551ac92da3
floor-vds <floor.vandesteene@gmail.com>
2020-05-02 18:00:00 +0200

    add landuse legend

--- vlinder/src/components/AreaStation.vue
+++ vlinder/src/components/AreaStation.vue
@@ -1,6 +1,8 @@
 <template>
-    <div id="d3-viz-area" style="height: 100%; width: 100%">
-    </div>
+    <b-row>
+        <b-col id="d3-viz-area" style="height: 100%; width: 80%"/>
+        <b-col id="d3-viz-area-legend" style="height: 100%; width: 20%"/>
+    </b-row>
 </template>
 
 
@@ -22,6 +24,7 @@
 
         mounted() {
             this.div = d3.select('#d3-viz-area');
+            this.legend = d3.select('#d3-viz-area-legend');
             let observer = new ResizeObserver(this.create_area_chart);
             observer.observe(this.div.node());
 
@@ -42,6 +45,7 @@
                     .attr("width", this.width)
                     .attr("height", this.height);
 
+                this.add_legend();
 
                 // setup everything
                 this.xLabels = ["0m", "20m", "50m", "100m", "250m", "500m"];
@@ -78,7 +82,7 @@
 
                 this.svg.select(".y.axis")
                     .append("text")
-                    .text('Landgebruik')
+                    .text('Omgeving')
                     .style("text-anchor", "end")
                     .attr("dx", -this.padding.top)
                     .attr("dy", "1em")
@@ -118,28 +122,28 @@
                 .attr('height', 10)
                 .attr('width', 100)
                 .attr('y', this.height/2-13)
-                .attr('x', this.width/2-115);
+                .attr('x', this.width/2-105);
             this.info_box.append("text")
-                .text(" Op de x-as straal waarin gekeken wordt. De hoogte ")
+                .text(" Op de x-as staat de straal waarin gekeken wordt.")
                 .style("font-size", "11px")
                 .attr('height', 10)
                 .attr('width', 100)
                 .attr('y', this.height/2-3)
-                .attr('x', this.width/2-115);
+                .attr('x', this.width/2-105);
             this.info_box.append("text")
-                .text(" van de rechthoek geeft weer hoeveel procent ervan ")
+                .text(" De hoogte van de rechthoek geeft weer hoeveel ")
                 .style("font-size", "11px")
                 .attr('height', 10)
                 .attr('width', 100)
                 .attr('y', this.height/2+7)
-                .attr('x', this.width/2-115);
+                .attr('x', this.width/2-105);
             this.info_box.append("text")
-                .text(" water, groen of verhard is. ")
+                .text(" procent ervan water, groen of verhard is. ")
                 .style("font-size", "11px")
                 .attr('height', 10)
                 .attr('width', 100)
                 .attr('y', this.height/2+17)
-                .attr('x', this.width/2-115);
+                .attr('x', this.width/2-105);
             // resize background box
             this.info_box.select("rect")
                 .attr("x", this.info_box.node().getBBox().x-5)
@@ -206,6 +210,93 @@
 
                 return this.svg.node();
             },
+
+            add_legend(){
+                let divBoxLegend = this.legend.node().getBoundingClientRect();
+                let w = divBoxLegend.width;
+                let h = divBoxLegend.height-20;
+
+                this.padding = {top: 20, left: 45, right: 20, bottom: 30};
+                this.legend.selectAll("*").remove();
+
+
+                this.svg_legend = this.legend.append("svg", 0)
+                    .attr("width", this.width)
+                    .attr("height", this.height);
+
+
+                // legend box
+                this.legend_box = this.svg_legend
+                    .append("g")
+                    .attr('y', this.padding.top)
+                    .attr('x', this.padding.left)
+                    .attr('width', w)
+                    .attr('height', h);
+                this.legend_box.append("rect")
+                    .attr("class", "legend")
+                    .attr("fill", "none")
+                    .attr("rx", '1')
+                    .attr("ry", '1')
+                    .attr('stroke', 'black');
+
+
+                //add colored squares
+                this.legend_box.append("rect")
+                    .attr('height', 10)
+                    .attr('width', 10)
+                    .attr('y',h/2-25)
+                    .attr('x', w/2-30)
+                    .attr("rx", '1')
+                    .attr("ry", '1')
+                    .attr("fill", 'lightskyblue' );
+                this.legend_box.append("rect")
+                    .attr('height', 10)
+                    .attr('width', 10)
+                    .attr('y',h/2-5)
+                    .attr('x', w/2-30)
+                    .attr("rx", '1')
+                    .attr("ry", '1')
+                    .attr("fill", 'limegreen' );
+                this.legend_box.append("rect")
+                    .attr('height', 10)
+                    .attr('width', 10)
+                    .attr('y',h/2+15)
+                    .attr('x', w/2-30)
+                    .attr("rx", '1')
+                    .attr("ry", '1')
+                    .attr("fill", 'saddlebrown' );
+
+                // add text
+                this.legend_box.append("text")
+                    .text(" water")
+                    .style("font-size", "11px")
+                    .attr('height', 10)
+                    .attr('width', 100)
+                    .attr('y',h/2-15)
+                    .attr('x', w/2-15);
+                this.legend_box.append("text")
+                    .text(" groen")
+                    .style("font-size", "11px")
+                    .attr('height', 10)
+                    .attr('width', 100)
+                    .attr('y',h/2+5)
+                    .attr('x', w/2-15);
+                this.legend_box.append("text")
+                    .text(" verhard")
+                    .style("font-size", "11px")
+                    .attr('height', 10)
+                    .attr('width', 100)
+                    .attr('y',h/2+25)
+                    .attr('x', w/2-15);
+
+                // resize background box
+                this.legend_box.select("rect")
+                    .attr("x", this.legend_box.node().getBBox().x - 5)
+                    .attr("y", this.legend_box.node().getBBox().y - 3)
+                    .attr("width", this.legend_box.node().getBBox().width + 10)
+                    .attr("height", this.legend_box.node().getBBox().height + 7);
+
+            }
         }
     }
 </script>
