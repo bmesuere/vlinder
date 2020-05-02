67736fe10a5464255dbc4daee5ff0bccd1952cb7
floor-vds <floor.vandesteene@gmail.com>
2020-05-02 15:08:30 +0200

    show inforbox

--- vlinder/src/components/AreaStation.vue
+++ vlinder/src/components/AreaStation.vue
@@ -83,6 +83,9 @@
                     .attr("dx", -this.padding.top)
                     .attr("dy", "1em")
                     .attr("transform", "rotate(-90)")
+                    .on("mouseover", () => {this.info_box.raise();
+                                            this.info_box.style("display", null)})
+                    .on("mouseout", () => {this.info_box.style("display", "none")})
                     .attr("fill", "black");
 
                 this.xAxisGroup.call(this.xAxis)
@@ -93,12 +96,64 @@
                     .attr("transform", "rotate(-45)");
 
                 this.yAxisGroup.call(this.yAxis);
-                this.update_data()
+
+
+            // infobox
+            this.info_box = this.svg
+                .append("g")
+                .attr('y', this.height/2-20)
+                .attr('x', this.width/2-50)
+                .style('display', 'none');
+            this.info_box.append("rect")
+                .attr("class", "legend-background")
+                .attr("fill", "#d3d3d3")
+                .attr("opacity", 0.9)
+                .attr("rx", '3')
+                .attr("ry", '3')
+                .attr('stroke', 'black');
+            // add text
+            this.info_box.append("text")
+                .text(" Weergave van de omgeving rond het weerstation. ")
+                .style("font-size", "11px")
+                .attr('height', 10)
+                .attr('width', 100)
+                .attr('y', this.height/2-13)
+                .attr('x', this.width/2-115);
+            this.info_box.append("text")
+                .text(" Op de x-as straal waarin gekeken wordt. De hoogte ")
+                .style("font-size", "11px")
+                .attr('height', 10)
+                .attr('width', 100)
+                .attr('y', this.height/2-3)
+                .attr('x', this.width/2-115);
+            this.info_box.append("text")
+                .text(" van de rechthoek geeft weer hoeveel procent ervan ")
+                .style("font-size", "11px")
+                .attr('height', 10)
+                .attr('width', 100)
+                .attr('y', this.height/2+7)
+                .attr('x', this.width/2-115);
+            this.info_box.append("text")
+                .text(" water, groen of verhard is. ")
+                .style("font-size", "11px")
+                .attr('height', 10)
+                .attr('width', 100)
+                .attr('y', this.height/2+17)
+                .attr('x', this.width/2-115);
+            // resize background box
+            this.info_box.select("rect")
+                .attr("x", this.info_box.node().getBBox().x-5)
+                .attr("y", this.info_box.node().getBBox().y-3)
+                .attr("width", this.info_box.node().getBBox().width+10)
+                .attr("height", this.info_box.node().getBBox().height+7);
+
+
+            this.update_data()
 
 
             },
             update_data() {
-                this.svg.selectAll("rect").remove();
+                this.svg.selectAll("rect.bar").remove();
 
                 if (this.selectedStations === undefined
                     || this.selectedStations.length === 0
@@ -123,53 +178,31 @@
                 };
 
                 let bars = this.svg
-                    .selectAll("rect")
+                    .selectAll("rect.bar")
                     .data(landUse)
                     .enter()
                     .append("g")
                     .classed('rect', true);
 
-                bars.append("rect")
+                const colors = ['lightskyblue', 'limegreen', 'saddlebrown'];
+                const types = ['water', 'groen', 'verhard'];
+                for(let k = 0; k < landUse[0]['usage'].length; k++){
+                    bars.append("rect")
+                    .attr('class', 'bar')
                     .attr("x", (d, i) => this.xScale(i + 0.6))
-                    .attr("y", this.padding.top)
+                    .attr("y", (d) => {
+                        let y = this.padding.top;
+                        for(let j = 0; j< k; j++){
+                            y+=this.dataScale(d['usage'][j]['value'])}
+                        return y;
+                    })
                     .attr("width", this.width / (this.xLabels.length + 1) * 0.8)
-                    .attr("height", d => this.dataScale(d['usage'][0]['value']))
-                    .attr("fill", "lightskyblue")
-                    .on("mouseover", function (d) {
-                        showLabel(this, d['usage'][0]['value'], 'water');
-                    })
-                    .on("mouseout", function () {
-                        removeLabel(this);
-                    })
+                    .attr("height", d => this.dataScale(d['usage'][k]['value']))
+                    .attr("fill", colors[k])
+                    .on("mouseover", function (d) { showLabel(this, d['usage'][k]['value'], types[k]); })
+                    .on("mouseout", function () { removeLabel(this); })
                 ;
-
-                bars.append("rect")
-                    .attr("x", (d, i) => this.xScale(i + 0.6))
-                    .attr("y", d => this.padding.top + this.dataScale(d['usage'][0]['value']))
-                    .attr("width", this.width / (this.xLabels.length + 1) * 0.8)
-                    .attr("height", d => this.dataScale((d['usage'][1]['value'])))
-                    .attr("fill", "limegreen")
-                    .on("mouseover", function (d) {
-                        showLabel(this, d['usage'][1]['value'], 'groen');
-                    })
-                    .on("mouseout", function () {
-                        removeLabel(this);
-                    })
-                ;
-
-                bars.append("rect")
-                    .attr("x", (d, i) => this.xScale(i + 0.6))
-                    .attr("y", d => this.padding.top + this.dataScale(d['usage'][0]['value']) + this.dataScale(d['usage'][1]['value']))
-                    .attr("width", this.width / (this.xLabels.length + 1) * 0.8)
-                    .attr("height", d => this.dataScale(d['usage'][2]['value']))
-                    .attr("fill", "saddlebrown")
-                    .on("mouseover", function (d) {
-                        showLabel(this, d['usage'][2]['value'], 'verhard')
-                    })
-                    .on("mouseout", function () {
-                        removeLabel(this);
-                    })
-                ;
+                }
 
                 return this.svg.node();
             },
