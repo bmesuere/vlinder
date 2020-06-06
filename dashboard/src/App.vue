2a22abb75a181174c26c8f7923332c280a4f7efb
Bart Mesuere <Bart.Mesuere@UGent.be>
2020-06-06 18:47:53 +0200

    remove boilerplate

--- dashboard/src/App.vue
+++ dashboard/src/App.vue
@@ -1,57 +1,37 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <v-app-bar app color="primary" dark dense>
+      <v-toolbar-title>VLINDER</v-toolbar-title>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+      <v-spacer />
 
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
+      <v-btn href="http://vlinder.ugent.be" target="_blank" text >
+        <span class="mr-2">VLINDER website</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <v-container>
+        <v-row class="text-center">
+          <v-col cols="12">
+            <StationsMap />
+          </v-col>
+        </v-row>
+      </v-container>
     </v-content>
   </v-app>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import StationsMap from './components/StationsMap.vue'
 
 export default Vue.extend({
   name: 'App',
 
   components: {
-    HelloWorld
+    StationsMap
   },
 
   data: () => ({
---
+++ dashboard/src/components/StationsMap.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="map">Hello world</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'Map',
+
+  data: () => ({
+    //
+  })
+})
+</script>
