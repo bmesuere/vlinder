92a1cd277e931f3042603faf93b863f4c4852c74
Bart Mesuere <Bart.Mesuere@UGent.be>
2020-06-07 15:12:56 +0200

    disable PWA for now

--- dashboard/src/registerServiceWorker.ts
+++ dashboard/src/registerServiceWorker.ts
@@ -2,7 +2,8 @@
 
 import { register } from 'register-service-worker';
 
-if (process.env.NODE_ENV === 'production') {
+// eslint-disable-next-line no-constant-condition
+if (false && process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
     ready () {
       console.log(
