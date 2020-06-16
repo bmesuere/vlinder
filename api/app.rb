e084ee0b00f3d59a81e77773971a8a6d1f4783bc
Rien Maertens <rien.maertens@posteo.be>
2020-06-16 12:22:21 +0200

    cors & route skeleton

--- api/app.rb
+++ api/app.rb
@@ -1,14 +1,53 @@
+#
+#  ██    ██ ██      ██ ███    ██ ██████  ███████ ██████ 
+#  ██    ██ ██      ██ ████   ██ ██   ██ ██      ██   ██ 
+#  ██    ██ ██      ██ ██ ██  ██ ██   ██ █████   ██████  
+#   ██  ██  ██      ██ ██  ██ ██ ██   ██ ██      ██   ██ 
+#    ████   ███████ ██ ██   ████ ██████  ███████ ██   ██ 
+#
+#
+#             █████  ██████  ██     __ .  . __
+#            ██   ██ ██   ██ ██    (o.\ \/ /.o)
+#            ███████ ██████  ██     \__\/\/__/
+#            ██   ██ ██      ██     /O /==\ O\
+#            ██   ██ ██      ██    (;O/ \/ \O;)
+#                   
+
+#
+# Configuration
+#
 
 configure do
+  set :allow_origin, "*"
+  set :allow_headers, "content-type,if-modified-since"
 end
 
 configure :development do
+  # Automatically reload when files change
   register Sinatra::Reloader
   after_reload do
     puts '=== Sinatra reloaded ==='
   end
 end
 
+#
+# Routes
+#
+
 get '/' do
-  json hello: 'world'
+  json stations: request.url + 'stations',
+       measurements: request.url + 'measurements'
 end
+
+get '/stations' do
+end
+
+get '/stations/:id' do
+end
+
+get '/measurements' do
+end
+
+get '/measurements/:id' do
+end
+
