88132796becb2e473a0f84de29a4a5e30b7df511
Rien Maertens <rien.maertens@posteo.be>
2020-06-16 14:17:03 +0200

    connect to database and read stations csv

--- api/app.rb
+++ api/app.rb
@@ -17,6 +17,9 @@
 # Configuration
 #
 
+$db_config_file = 'login.json'
+$stations_csv_file = 'data.csv'
+
 configure do
   set :allow_origin, "*"
   set :allow_headers, "content-type,if-modified-since"
@@ -31,8 +34,55 @@ configure :development do
 end
 
 #
+# Helpers
+#
+
+def connect_db
+  config = JSON.parse(File.read($db_config_file)).transform_keys(&:to_sym)
+  config[:connect_timeout] = 2 # crash if we can't connect within 2 seconds
+  Mysql2::Client.new(config)
+end
+
+def read_stations
+  stations = {}
+  CSV.foreach($stations_csv_file, headers: true) do |row|
+    stations[row['ID']] = {
+      id: row['ID'],
+      name: row['VLINDER'],
+      coords: { lat: row['lat'].to_f, lon: row['lon'].to_f },
+      city: row['stad'],
+      given_name: row['benaming'],
+      landUse: [20, 50, 100, 250, 500].map { |distance|
+        {
+          distance: distance,
+          usage: ['water', 'verhard', 'groen'].map { |type|
+            { type: type, value: row["#{type}#{distance}"].to_f }
+          }
+        }
+      }
+    }
+  end
+  stations
+end
+
+#
+# Setup
+#
+
+$stations = read_stations
+$db = connect_db
+
+#
 # Routes
 #
+
+not_found do
+  json error: 'not found'
+end
+
+error do
+  json error: 'the server is on fire'
+end
 
 get '/' do
   json stations: request.url + 'stations',
@@ -40,9 +90,12 @@ get '/' do
 end
 
 get '/stations' do
+  json $stations
 end
 
 get '/stations/:id' do
+  pass if $stations[params['id']].nil?
+  json $stations[params['id']]
 end
 
 get '/measurements' do
@@ -50,4 +103,3 @@ end
 
 get '/measurements/:id' do
 end
-
