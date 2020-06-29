472a2960ccbb71a825947daab172f62494019b91
bmesuere <mesuerebart@gmail.com>
2020-06-29 16:58:27 +0000

    Deploying to gh-pages from  @ bb189b483558b648166ff9e04f94c81797e1d3f1 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/vlinder/precache-manifest.fcb5b8baa4fc84d3d21fcf8c84fe82b9.js"
+  "/vlinder/precache-manifest.9f5fdfcca9ebbe33b0935ee0d4296563.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "vlinder-dashboard"});
