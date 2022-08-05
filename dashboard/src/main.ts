640841505f7ef2fcb5c2e76ac89fe3aa9e415a74
Bart Mesuere <Bart.Mesuere@UGent.be>
2022-08-05 12:15:21 +0200

    install pinia

--- dashboard/src/main.ts
+++ dashboard/src/main.ts
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import App from './App.vue';
+import { createPinia, PiniaVuePlugin } from 'pinia';
 import VueGtag from 'vue-gtag';
 import './registerServiceWorker';
 import vuetify from './plugins/vuetify';
@@ -10,12 +11,16 @@ import store from './store';
 
 Vue.config.productionTip = false;
 
+Vue.use(PiniaVuePlugin);
+const pinia = createPinia();
+
 Vue.use(VueGtag, {
   config: { id: 'UA-170459792-1' }
 }, router);
 
 new Vue({
   vuetify,
+  pinia,
   store,
   router,
   render: h => h(App)
