5206b45720ff8d106bb24c8ed1d2d9fa6ba72ee8
Bart Mesuere <Bart.Mesuere@UGent.be>
2022-08-05 16:47:39 +0200

    fix checked checkbox

--- dashboard/src/components/StationSelector.vue
+++ dashboard/src/components/StationSelector.vue
@@ -1,49 +1,27 @@
 <template>
-  <v-dialog
-    v-model="dialog"
-    scrollable
-    max-width="500"
-    transition="dialog-bottom-transition"
-  >
+  <v-dialog v-model="dialog" scrollable max-width="500" transition="dialog-bottom-transition">
     <template v-slot:activator="{ on, attrs }">
-      <v-btn
-        color="primary"
-        dark
-        v-bind="attrs"
-        v-on="on"
-      >
+      <v-btn color="primary" dark v-bind="attrs" v-on="on">
         Selecteer stations
       </v-btn>
     </template>
     <v-card class="mx-auto" max-width="500">
-      <v-card-title><v-text-field
-        v-model="search" label="Typ om te filteren" prepend-inner-icon="mdi-magnify"
-        hide-details
-        autofocus
-        clearable
-      >
-      </v-text-field>
+      <v-card-title>
+        <v-text-field v-model="search" label="Typ om te filteren" prepend-inner-icon="mdi-magnify" hide-details
+          autofocus clearable>
+        </v-text-field>
 
       </v-card-title>
 
       <v-card-text style="height: 300px;">
-        <v-treeview
-          v-on:update:active="selectStation"
-          :items="stations"
-          :search="search"
-          :filter="filter"
-          :active="activeStations"
-          activatable
-          hoverable
-          multiple-active
-          open-on-click
-        >
+        <v-treeview v-on:update:active="selectStation" :items="stations" :search="search" :filter="filter"
+          :active="activeStations" activatable hoverable multiple-active open-on-click>
           <template v-slot:label="{ item }">
             {{ item.city + " &middot; " + item.given_name }}
           </template>
           <template v-slot:prepend="{ active }">
-            <v-icon >
-              {{ active ? 'mdi-check-box-outline' : 'mdi-checkbox-blank-outline' }}
+            <v-icon>
+              {{ active ? 'mdi-checkbox-outline' : 'mdi-checkbox-blank-outline' }}
             </v-icon>
           </template>
         </v-treeview>
@@ -51,11 +29,7 @@
       <v-divider></v-divider>
       <v-card-actions>
         <v-spacer></v-spacer>
-        <v-btn
-          color="blue darken-1"
-          text
-          @click="dialog = false"
-        >
+        <v-btn color="blue darken-1" text @click="dialog = false">
           Sluiten
         </v-btn>
       </v-card-actions>
