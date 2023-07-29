7f827dbc4d663f53f2ed3291cb018d99b8fb8c7b
Bart Mesuere <Bart.Mesuere@UGent.be>
2023-07-29 08:30:13 +0000

    try fixing the router

--- dashboard/src/router/index.ts
+++ dashboard/src/router/index.ts
@@ -20,7 +20,8 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  //history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory('/vlinder/'),
   routes,
 })
 
