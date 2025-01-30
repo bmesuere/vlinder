bd40c2507387191fe5188717f7260e829b80d97c
Thomas Vergauwen <thomas.vergauwen@meteo.be>
2025-01-30 13:31:10 +0100

    check for uniqe id and name

--- scripts/format_data.py
+++ scripts/format_data.py
@@ -16,7 +16,7 @@ def write_vlinder_data_to_csv(data, data_path):
     #check for duplicates
     uniqueColumns=['ID', 'VLINDER']
     for uniquecol in uniqueColumns:
-        assert data[uniquecol].duplicated().any(), f'Duplicate values found in data column: {uniquecol}'
+        assert not data[uniquecol].duplicated().any(), f'Duplicate values found in data column: {uniquecol}'
 
     data.to_csv(path_or_buf = data_path,
                 sep=',',
